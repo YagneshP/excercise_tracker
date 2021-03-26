43740b19b9683ae53d64334046ffab7b39cdd113
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-26 18:03:05 -0400

    feat: connect to the database

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 require('dotenv').config()
-
+const mongoose = require("mongoose")
 app.use(cors())
 app.use(express.static('public'))
-
+app.use(express.json());
+app.use(express.urlencoded());
+//mongoDB connection
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,useUnifiedTopology: true}).
+  catch(error => console.log(error));
 // Routes
 //Home Page
 app.get('/', (req, res) => {
