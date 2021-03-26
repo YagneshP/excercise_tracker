31630d4e0f8f03ed931d0485e060e5639e9b060d
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-26 17:39:01 -0400

    feat: add route stucture for app

--- server.js
+++ server.js
@@ -5,10 +5,31 @@ require('dotenv').config()
 
 app.use(cors())
 app.use(express.static('public'))
+
+// Routes
+//Home Page
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+// POST register/create new User 
+app.post("/api/excersice/new-user",(req,res)=>{
+	res.send("New User route")
+});
 
+//GET getting all users
+app.get("/api/excercise/users", (req,res)=>{
+	res.send("Send array with objects containing username and _id property")
+});
+
+//POST add excercise
+app.post("/api/excercise/add",(req,res)=>{
+	res.send("User object with excercise field")
+})
+
+//GET full excercise log of user 
+app.get("/api/excercise/log/:user_id",(req,res)=>{   // from to date in yyyy-mm-dd format and limit
+	res.send("User with log array with description duration and date also a count property")
+})
 
 
 
