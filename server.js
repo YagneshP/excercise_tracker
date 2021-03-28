0f8ca3cb458df92c033096c44d1e710398363453
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-27 20:04:54 -0400

    feat: add logic to create excercise to particular user

--- server.js
+++ server.js
@@ -4,15 +4,16 @@ const cors = require('cors')
 require('dotenv').config()
 const mongoose = require("mongoose")
 const User = require("./models/user")
-const Excercise = require("./models/excercise")
+// const Excercise = require("./models/excercise")
 
 app.use(cors())
 app.use(express.static('public'))
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 //mongoDB connection
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,useUnifiedTopology: true}).
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true,useUnifiedTopology: true,useFindAndModify:false}).
   catch(error => console.log(error));
+	mongoose.set('returnOriginal', false);
 // Routes
 //Home Page
 app.get('/', (req, res) => {
@@ -48,8 +49,24 @@ app.get("/api/exercise/users", async(req,res)=>{
 });
 
 //POST add exercise
-app.post("/api/exercise/add",(req,res)=>{
-	res.send("User object with exercise field")
+app.post("/api/exercise/add",async(req,res)=>{
+	try{
+		const{userId, description, date, duration} = req.body
+	
+		let foundUser = await User.findByIdAndUpdate(userId, {$push:{log:{
+			userId,
+			description,
+			date,
+			duration
+		}}});
+		if(foundUser){
+		return 	res.status(200).json(foundUser)
+		}else{
+			throw Error("User not found")
+		}
+	}catch(error){
+		return 	res.json({error:error.message})
+	}
 })
 
 //GET full exercise log of user 
