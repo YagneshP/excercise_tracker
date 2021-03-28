3e284f21047714835fbe6a64054535e8985b06ac
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-03-27 20:06:13 -0400

    style: commented out whole file

--- models/excercise.js
+++ models/excercise.js
@@ -1,24 +1,24 @@
-const mongoose = require("mongoose")
-const User = require("./user")
-const excerciseSchema = new mongoose.Schema({
-	userId:{
-		type: mongoose.Schema.Types.ObjectId,
-		ref:"User"
-	},
-	description:{
-		type: String,
-		required:true
-	},
-	duration:{
-		type:Number,
-		required:true
-	},
-	date:{
-		type:Date,
-		default: Date.now
-	}
-});
+// const mongoose = require("mongoose")
+// const User = require("./user")
+// const excerciseSchema = new mongoose.Schema({
+// 	userId:{
+// 		type: mongoose.Schema.Types.ObjectId,
+// 		ref:"User"
+// 	},
+// 	description:{
+// 		type: String,
+// 		required:true
+// 	},
+// 	duration:{
+// 		type:Number,
+// 		required:true
+// 	},
+// 	date:{
+// 		type:Date,
+// 		default: Date.now
+// 	}
+// });
 
-const excercise = mongoose.model("Excercise", excerciseSchema);
+// const excercise = mongoose.model("Excercise", excerciseSchema);
 
-module.exports = excercise;
+// module.exports = {excercise, excerciseSchema};
